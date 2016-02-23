18e27cd949ba99a98ff5d792c11a7e9c48430e5f
Andre Luiz <atec.developer@gmail.com>
2016-02-23 20:33:33 +0000

    Array samples

---
+++ arrays/exercise2.rb
@@ -0,0 +1,2 @@
+arr = [["test", "hello", "world"],["example", "mem"]]
+puts arr.last.first #brings the last element and then brings the first from the last one
---
+++ arrays/exercise3.rb
@@ -0,0 +1,6 @@
+arr = [1, 2, 3, 4, 5]
+
+arr2 = arr.map {|i| i + 2} #map interates the array then returns a new array
+
+p arr
+p arr2
