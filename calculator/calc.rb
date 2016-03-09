f8e89971a09f7e7e330da210841a77bdb3d6b417
Andre Luiz <atec.developer@gmail.com>
2016-03-09 04:34:31 +0000

    Calculator example after rubocop edition

--- calculator/calc.rb
+++ calculator/calc.rb
@@ -1,31 +1,23 @@
-#Calculator example
-num1 = nil
-num2 = nil
-operation = ""
-result = nil
-run_calc = "n"
+# Calculator example
+operation = ''
+run_calc = 'n'
 
-#Performs the requested calculation
-def calculate (op, x, y)
-  begin
-    result = case op 
-    when "plus" 
-      result = x + y
-    when "minus"
-      result = x - y
-    when "multiple"
-      result = x * y
-    when "divide"
-      result = x / y
-    end
-  rescue Exception => e
-    puts "Something went wrong!"
-    puts e.message
+# Performs the requested calculation
+def calculate(op, x, y)
+  case op
+  when 'plus'
+    x + y
+  when 'minus'
+    x - y
+  when 'multiple'
+    x * y
+  when 'divide'
+    x / y
   end
 end
 
 while run_calc != "y"
-  puts "---------- Calc v1.0 ----------"
+  puts "---------- Calc v2.0 ----------"
   puts "Choose first number"
   num1 = gets.to_f
   puts "Choose second number"
@@ -33,20 +25,15 @@ while run_calc != "y"
   puts "Choose operation (plus/minus/divide/multiple)"
   operation = gets.chomp.downcase
 
-  while operation != "plus" && operation != "minus" && operation != "divide" && operation != "multiple"   
+  while operation != "plus" && operation != "minus" && operation != "divide" && operation != "multiple"
     puts "Operation invalid"
     puts "Choose operation (plus/minus/divide/multiple)"
     operation = gets.chomp.downcase
   end
-  
+
   puts "-------------------------------"
   puts "#{num1} #{operation} #{num2} = #{calculate(operation, num1, num2)}"
   puts "-------------------------------"
-  
   puts "Do you want to exit? (y/n)"
   run_calc = gets.chomp.downcase
 end
-
-
-
-
